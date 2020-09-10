6792822bfd82b8331f03e6e01190b9c485c56968
Roberto Alvarez <roberto.alvarez@fidumtec.com>
2020-09-10 19:07:15 -0300

    cors

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 // const https = require('https');
 // const fs = require('fs');
 
@@ -15,13 +16,15 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 // Enable CORS Origin
+/*
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method, token');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
-});
+});*/
+app.use(cors());
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
