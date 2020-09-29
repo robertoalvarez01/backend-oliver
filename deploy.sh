77a2c59319338d652e4d1771dd4206403bf1e05a
root <root@localhost.localdomain>
2020-09-29 13:38:01 -0300

    Evita el deploy de la MongoDB

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@
 #Es necesario un volume con el nombre "oliverpetshop"
 
 docker stop backend-oliver
-docker stop petshop-oliver-mongo
-docker container rm petshop-oliver-mongo
+#docker stop petshop-oliver-mongo
+#docker container rm petshop-oliver-mongo
 docker container rm backend-oliver
-docker image rm backend-oliver
+#docker image rm backend-oliver
 
 docker build -t backend-oliver .
 
-docker run -d --restart unless-stopped --name petshop-oliver-mongo -p 27017:27017 --mount src=oliverpetshop,dst=/data/db mongo
-docker run -d --restart unless-stopped --name backend-oliver -p 3000:3000 --link petshop-oliver-mongo backend-oliver
+#docker run -d --restart unless-stopped --name petshop-oliver-mongo -p 27017:27017 --mount src=oliverpetshop,dst=/data/db mongo
+docker run -d --restart unless-stopped --name backend-oliver -p 3000:3000 backend-oliver
