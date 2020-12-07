5e460c38e0169190edceca5f75cb16e3b7b52d57
root <root@localhost.localdomain>
2020-12-07 15:00:14 -0300

    update

--- server/routes/mercadopago.js
+++ server/routes/mercadopago.js
@@ -4,7 +4,12 @@ const MercadoPagoService = require('../services/MercadoPago');
 
 app.post('/mercadopago',async(req,res,next)=>{
     const mercadopago = new MercadoPagoService();
-    const response = await mercadopago.init();
+    const response = await mercadopago.init([{
+        "title": "Dummy Item",
+        "description": "Multicolor Item",
+        "quantity": 1,
+        "unit_price": 10.0
+    }]);
     res.status(200).json({
         info:response
     })
