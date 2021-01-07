7f1c03ad271a42a71968608fc485d5dfa8978fce
root <root@localhost.localdomain>
2021-01-07 16:43:10 -0300

    update

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -226,7 +226,7 @@ app.put('/actualizarDireccion/:id',verificarToken,async(req,res)=>{
                 foto:updatedUser[0].foto,
                 address:updatedUser[0].address,
                 idUsuario:updatedUser[0].idUsuario,
-                token:token,
+                token:updatedUser[0].token,
                 lat:updatedUser[0].lat,
                 lon:updatedUser[0].lon
             }
