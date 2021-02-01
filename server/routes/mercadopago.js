55d8553dba8508a5bca318b56dc69576c8ecbe9c
Franco Benitez <fhbenitez@hcdn.gob.ar>
2021-02-01 17:07:46 -0300

    update mercado pago

--- server/routes/mercadopago.js
+++ server/routes/mercadopago.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const app = express();
 const MercadoPagoService = require('../services/MercadoPago');
+const { verificarToken } = require('../middlewares/autenticacion');
 
-app.post('/mercadopago',async(req,res,next)=>{
-    const mercadopago = new MercadoPagoService();
-    const response = await mercadopago.init([{
-        "title": "Dummy Item",
-        "description": "Multicolor Item",
-        "quantity": 1,
-        "unit_price": 10.0
-    }]);
-    res.status(200).json({
-        info:response
-    })
+app.post('/mercadopago',verificarToken,async(req,res,next)=>{
+    const {productos} = req.body;
+    if(!productos || productos.length==0) return res.status(500).json({
+        ok:false,
+        info:'Ningun producto recibido'
+    });
+    try {
+        const mercadopago = new MercadoPagoService();
+        const response = await mercadopago.init(productos);
+        res.status(200).json({
+            info:response
+        })
+    } catch (error) {
+        res.status(500).json({
+            ok:false,
+            error:error.message
+        })
+    }
 });
 
 module.exports = app;
