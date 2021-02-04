fa0e28f35180c02b5ee3048b8c223ff4499b4db0
root <root@localhost.localdomain>
2021-02-04 14:31:58 -0300

    update

--- server/routes/ventas.js
+++ server/routes/ventas.js
@@ -13,7 +13,14 @@ app.post('/registrarVenta',[verificarToken],async(req,res)=>{
         const {envio:dataEnvio,venta:dataVenta} = req.body;
         if(!dataEnvio.idZona || dataEnvio.tipo==''){
 		    return res.status(500).json({ok:false,error:'Datos de envio insuficientes.'});
+        }else if(dataVenta.productos.length==0){
+            return res.status(500).json({ok:false,error:'No hay productos para registrar de la venta'})
         }else{
+            //antes de registrar algo, verifico el id de la operacion para descartar que sea una operacion duplicada
+            const verificarOperacion = await vService.verificarVentaDuplicada(dataVenta.operacion_id);
+            if(verificarOperacion.length>0){
+                return res.status(500).json({ok:false,info:'Ha ocurrido un error con la operación, lo sentimos mucho.'})
+            }
             const newEnvio = await eService.create(dataEnvio);
             const {idEnvio:idUltimoEnvio} = newEnvio[0];
 
